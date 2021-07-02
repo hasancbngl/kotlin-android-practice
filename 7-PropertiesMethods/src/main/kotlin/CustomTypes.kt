d65833cae27868e245437ac7c6261ae0f7db37bd
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2021-07-02 14:06:44 +0300

    add companion object examples

--- 7-PropertiesMethods/src/main/kotlin/CustomTypes.kt
+++ 7-PropertiesMethods/src/main/kotlin/CustomTypes.kt
@@ -6,26 +6,61 @@ class CustomTypes {
         val fullName = "$firstName $lastName"
     }
 
-    class TV(var width: Double, var height: Double) {
-        var diagnal: Int
-            get() {
+    class TVMath() {
+        companion object {
+            fun getDiagnol(width: Double, height: Double): Int {
                 val result = sqrt(width * width + height * height)
                 return result.roundToInt()
             }
-            set(value) {
-                val ratioWidth = 16.0
-                val ratioHeight = 9.0
+
+            fun getWidthAndHeight(diagnol: Int, ratioWidth: Double, ratioHeight: Double): Pair<Double, Double> {
                 val ratioDiagnoal = sqrt(ratioWidth * ratioWidth + ratioHeight * ratioHeight)
-                height = value.toDouble() * ratioHeight / ratioDiagnoal
-                width = height * ratioWidth / ratioHeight
+                val height = diagnol.toDouble() * ratioHeight / ratioDiagnoal
+                val width = height * ratioWidth / ratioHeight
+                return Pair(width, height)
             }
+        }
+    }
+
+    class TV(var width: Double, var height: Double) {
+        var diagnal: Int
+            get() {
+                return TVMath.getDiagnol(width, height)
+            }
+            set(value) {
+                val (widthSize, heightSize) = TVMath.getWidthAndHeight(
+                    diagnol = value,
+                    ratioWidth = 16.0,
+                    ratioHeight = 9.0
+                )
+                width = widthSize
+                height = heightSize
+            }
+    }
+
+    class Level(
+        val id: Int,
+        val boss: String,
+        var unlocked: Boolean
+    ) {
+        companion object {
+            //highesr level property on level itself so can not be accesed from instance
+            var highestLevel = 1
+        }
     }
 
     companion object {
 
         fun costomType() {
-            val grace = Person("Grace", "Hopper")
-            println(grace.fullName)
+/*            val grace = Person("Grace", "Hopper")
+            println(grace.fullName)*/
+
+            val level1 = Level(1, "Outside Cat", true)
+            val level2 = Level(id = 2, boss = "Laser Pointer", unlocked = false)
+            val level3 = Level(id = 3, boss = "Mysterious Attic Sound", unlocked = false)
+            val level4 = Level(id = 4, boss = "Vacuum Cleaner", unlocked = false)
+
+            println("Highest LEvel is: ${Level.highestLevel}")
 
             val tv = TV(width = 85.3, height = 45.454)
             println(tv.diagnal)
