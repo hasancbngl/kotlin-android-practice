b475222636b72e00c62a72ad3cd49c75b47dd3aa
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2021-07-04 11:48:52 +0300

    add Inheritance starter

---
+++ 8-ClassInheritanceInterfaces/src/main/kotlin/Inheritance.kt
@@ -0,0 +1,20 @@
+
+data class Grade(val courseName: String, val letter: Char, val credits: Double)
+
+class Person(var firstName: String, var lastName: String) {
+    fun fullName() = "$firstName $lastName"
+}
+
+class Student(var firstName: String, var lastName: String,
+              var grades: MutableList<Grade> = mutableListOf<Grade>()) {
+
+    fun fullName() = "$firstName $lastName"
+
+    fun recordGrade(grade: Grade) {
+        grades.add(grade)
+    }
+}
+
+fun main() {
+
+}
